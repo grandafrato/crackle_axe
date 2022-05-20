ff15057921ac2db038a4a6670beeaa6f79aefd07
Lachlan Wilger <lachlan@wilgerfamily.com>
2022-05-20 13:05:11 -0700

    Add API functions to Attributes and a health attr.

--- lib/crackle_axe/data/attributes.ex
+++ lib/crackle_axe/data/attributes.ex
@@ -9,12 +9,39 @@ defmodule CrackleAxe.Data.Attributes do
     end
   end
 
+  # Attributes API
+
+  @doc """
+  Takes a map and a tuple with the key and item and inserts them into it.
+  """
+  @spec add_attribute(%{} | attrs(), {atom(), t()}) :: attrs()
+  def add_attribute(map, {name, item}), do: Map.put(map, name, item)
+
+  @spec apply_action(t(), atom(), list(any())) :: t()
+  def apply_action({actions, state}, action, args) do
+    {actions, apply(Map.get(actions, action), [state | args])}
+  end
+
+  # Attributes
+
   @doc """
   An example of a basic attribute.
   """
   @spec basic() :: {:basic, {%{atom() => fun()}, boolean()}}
-  def basic(), do: {:basic, {%{identity: & &1}, Integer.mod(Enum.random(1..100), 2) == 0}}
+  def basic(),
+    do:
+      {:basic,
+       {%{identity: & &1, opposite_identity: &(!&1)}, Integer.mod(Enum.random(1..100), 2) == 0}}
 
-  @spec add_attribute(map(), {atom(), t()}) :: attrs()
-  def add_attribute(map, {name, item}), do: Map.put(map, name, item)
+  @doc """
+  The attribute the measures health level.
+  """
+  @spec health(non_neg_integer(), non_neg_integer()) ::
+          {:health, {%{atom() => fun()}, non_neg_integer()}}
+  def health(max_health, current_health) do
+    {:health, {%{change_by: &max(0, min(max_health, &1 + &2))}, current_health}}
+  end
+
+  @spec health(non_neg_integer()) :: {:health, {%{atom() => fun()}, non_neg_integer()}}
+  def health(max_health), do: health(max_health, max_health)
 end
--- test/crackle_axe/data/attributes_test.exs
+++ test/crackle_axe/data/attributes_test.exs
@@ -2,18 +2,64 @@ defmodule CrackleAxe.Data.AttributesTest do
   use CrackleAxe.Data.Attributes
   use ExUnit.Case, async: true
 
+  describe "Attributes API" do
+    test "add_attribute/2" do
+      assert match?(%{basic: {_funs, _state}}, add_attribute(%{}, basic()))
+    end
+
+    test "apply_action/3" do
+      {:basic, {actions, state} = basic_action} = basic()
+
+      assert apply_action(basic_action, :identity, []) == {actions, actions.identity.(state)}
+
+      assert apply_action(basic_action, :opposite_identity, []) ==
+               {actions, actions.opposite_identity.(state)}
+    end
+  end
+
   describe "basic attribute" do
     test "it is a tuple of its name, functions and state" do
       assert match?({:basic, {_functions, _state}}, basic())
     end
 
-    test "basic function takes the state as an argument and returns it" do
+    test "identity function takes the state as an argument and returns it" do
       {:basic, {funs, state}} = basic()
       assert state == funs.identity.(state)
     end
+
+    test "opposite_identity function takes the state as an argument and returns the opposite of it" do
+      {:basic, {funs, state}} = basic()
+      assert !state == funs.opposite_identity.(state)
+    end
   end
 
-  test "add_attribute/2" do
-    assert match?(%{basic: {_funs, _state}}, add_attribute(%{}, basic()))
+  describe "health_attribute" do
+    test "health/2 will generate a halth attr with the expected current/msx health" do
+      assert match?({:health, {_funs, 73}}, health(100, 73))
+    end
+
+    test "health/1 is equal to health/2 with equal arguments" do
+      number = Enum.random(0..100)
+
+      assert health(number) == health(number, number)
+    end
+
+    test ":change_by will add or subtract the health level by the value given value" do
+      {:health, {actions, _} = health_attr} = health(182)
+
+      assert apply_action(health_attr, :change_by, [-2]) == {actions, 180}
+    end
+
+    test ":change_by will not excede the max value" do
+      {:health, {actions, _} = health_attr} = health(182, 90)
+
+      assert apply_action(health_attr, :change_by, [+190]) == {actions, 182}
+    end
+
+    test ":change_by will not go below the min value" do
+      {:health, {actions, _} = health_attr} = health(182, 90)
+
+      assert apply_action(health_attr, :change_by, [-190]) == {actions, 0}
+    end
   end
 end
