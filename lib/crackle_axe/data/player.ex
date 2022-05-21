a5188a3a9698720b2f7447e36d66b5015faf8c96
Lachlan Wilger <lachlan@wilgerfamily.com>
2022-05-20 21:51:31 -0700

    Add Player heal/damage.

--- lib/crackle_axe/data/player.ex
+++ lib/crackle_axe/data/player.ex
@@ -6,6 +6,34 @@ defmodule CrackleAxe.Data.Player do
     field :attributes, Attributes.attrs(), default: []
   end
 
+  @doc """
+  Creates a new valid player struct.
+  """
   @spec new() :: t()
-  def new(), do: %__MODULE__{attributes: add_attribute(%{}, basic())}
+  def new(),
+    do: %__MODULE__{attributes: %{} |> add_attribute(basic()) |> add_attribute(health(100))}
+
+  @doc """
+  Reduces the player's health.
+  """
+  @spec damage(t(), integer) :: t()
+  def damage(player, damage) do
+    Map.update!(
+      player,
+      :attributes,
+      &Map.update!(&1, :health, fn x -> apply_action(x, :change_by, [-damage]) end)
+    )
+  end
+
+  @doc """
+  Increase the player's health.
+  """
+  @spec heal(t(), integer) :: t()
+  def heal(player, damage) do
+    Map.update!(
+      player,
+      :attributes,
+      &Map.update!(&1, :health, fn x -> apply_action(x, :change_by, [damage]) end)
+    )
+  end
 end
--- test/crackle_axe/data/player_test.exs
+++ test/crackle_axe/data/player_test.exs
@@ -6,4 +6,25 @@ defmodule CrackleAxe.Data.PlayerTest do
     player = Player.new()
     assert Enum.any?(player.attributes, fn {name, {_funs, _state}} -> name == :basic end)
   end
+
+  describe "Player Health" do
+    test "has health attribute" do
+      player = Player.new()
+      assert Map.get(player.attributes, :health) != nil
+    end
+
+    test "damage/2 reduces player health by the amount given" do
+      player = Player.new()
+      assert match?(%Player{attributes: %{health: {_, 90}}}, Player.damage(player, 10))
+    end
+
+    test "heal/2 reduces player health by the amount given" do
+      player = Player.new()
+
+      assert match?(
+               %Player{attributes: %{health: {_, 94}}},
+               player |> Player.damage(10) |> Player.heal(4)
+             )
+    end
+  end
 end
